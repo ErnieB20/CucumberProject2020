fc0a289592674527b09455514b6cc90c44264770
ErnieB20 <ernie312us@gmail.com>
2020-10-25 15:25:35 -0500

    updated

--- test/java/com/vytrack/runners/CucumberRunner.java
+++ test/java/com/vytrack/runners/CucumberRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = "com/vytrack/step_definitions",
+        glue = "com/vytrack/step_definition",
         dryRun = false,
         tags = " @add_car",
         publish = true
